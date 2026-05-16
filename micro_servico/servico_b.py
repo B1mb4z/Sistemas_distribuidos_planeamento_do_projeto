3bf47ac3dce799bbb4132ea11c4b62db424959d3
bmbz <elvin007pires@gmail.com>
2026-05-16 12:07:51 -0100

    correcao dos micro servicos a & b

--- micro_servico/servico_b.py
+++ micro_servico/servico_b.py
@@ -4,24 +4,21 @@ app = Flask(__name__)
 pedidos = []
 
 @app.route('/pedidos', methods=['GET'])
-
 def get_pedidos():
     return jsonify(pedidos)
 
 @app.route('/pedidos', methods=['POST'])
-
 def add_pedido():
-    data = request.get_jsonify()
+    data = request.get_json()  # get_json(), não get_jsonify()
 
     if not data or 'item_id' not in data or 'quantidade' not in data:
-        return jsonify({'erro' : 'É obrigatório preencher os campos item_id e quantidade.'}), 400
-    
+        return jsonify({'erro': 'É obrigatório preencher item_id e quantidade.'}), 400
+
     novo_pedido = {
-        'id_pedido': len(pedidos + 1),
+        'id_pedido': len(pedidos) + 1,  # len(pedidos) + 1
         'id_item': data['item_id'],
         'quantidade': data['quantidade']
     }
-
     pedidos.append(novo_pedido)
     return jsonify(novo_pedido), 201
 
